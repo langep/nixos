0a7c8175bf947a135ba38c6dad052801c35233d0
Patrick Lange <patrick.l.lange@gmail.com>
2025-12-13 23:50:15 -0800

    Move system into flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "NixOS System Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      thinkpad = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+	modules = [
+	  ./hosts/thinkpad/default.nix
+	  inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
+	];
+      };
+    };
+  };
+}
