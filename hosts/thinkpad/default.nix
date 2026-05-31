c74d462a261ea3675c97b92e215ab480afae7c12
Patrick Lange <patrick.l.lange@gmail.com>
2026-05-31 01:08:28 -0700

    setup dns servers

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -18,7 +18,12 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Network
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    dns = "systemd-resolved";
+  };
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" "2606:4700:4700::1111"];
+  services.resolved.enable = true;
   networking.hostName = "thinkpad";
 
   # This value determines the NixOS release from which the default
