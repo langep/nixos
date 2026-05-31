4a0c2c565479b298b899ce686134494615e27207
Patrick Lange <patrick.l.lange@gmail.com>
2026-05-31 01:13:55 -0700

    add rebuild alias

--- home/fish.nix
+++ home/fish.nix
@@ -5,6 +5,9 @@
     interactiveShellInit = ''
       set fish_greeting
     '';
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch --flake /home/langep/nixos#$(hostname)";
+    };
   };
 
 }
