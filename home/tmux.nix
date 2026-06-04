cf7b21e1cf5a321daf99dff4d8e9113f1e004140
Patrick Lange <patrick.l.lange@gmail.com>
2026-06-04 02:25:54 -0700

    update tmux config

--- home/tmux.nix
+++ home/tmux.nix
@@ -44,10 +44,12 @@
         bind '"' split-window -v -c "#{pane_current_path}"
 
         # Status bar
+        # set -g status-position top
+        set -g status-style "bg=${c.base01}"
         set -g status-justify absolute-centre
-        set -g status-left "#[fg=${c.base00},bg=${c.base0D},bold] #S #[fg=${c.base0D},bg=default]${rightArrow} "
+        set -g status-left "#[fg=${c.base01},bg=${c.base0D}] #S #[fg=${c.base0D},bg=default]${rightArrow} "
         set -g window-status-format "#[fg=${c.base03},bg=default]${leftCap}#[fg=${c.base05},bg=${c.base03}] #I #W #[fg=${c.base03},bg=default]${rightCap}"
-        set -g window-status-current-format "#[fg=${c.base0D},bg=default]${leftCap}#[fg=${c.base00},bg=${c.base0D},bold] #I #W #[fg=${c.base0D},bg=default]${rightCap}"
+        set -g window-status-current-format "#[fg=${c.base0D},bg=default]${leftCap}#[fg=${c.base00},bg=${c.base0D}] #I #W #[fg=${c.base0D},bg=default]${rightCap}"
         set -g status-right ""
         set -g status-left-length 100
         set -g status-right-length 100
